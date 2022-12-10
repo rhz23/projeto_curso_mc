cdaa55d4fc19cd382d71198f3af76b3588eed56d
Rodrigo Zaninelli <rhzaninelli@gmail.com>
2022-12-10 17:36:59 -0300

    Paginação com parametros opcionais na requisição

--- src/main/java/com/rzaninelli/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/rzaninelli/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,7 @@ import com.rzaninelli.cursomc.domain.Categoria;
 import com.rzaninelli.cursomc.dto.CategoriaDTO;
 import com.rzaninelli.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +26,18 @@ public class CategoriaResource {
 
         List<Categoria> list = categoriaService.findAll();
         List<CategoriaDTO> listDto = list.stream().map(CategoriaDTO::new).toList();
+
+        return ResponseEntity.ok().body(listDto);
+    }
+
+    @RequestMapping(value = "/page", method = RequestMethod.GET)
+    public ResponseEntity<Page<CategoriaDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+                                                       @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+                                                       @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+                                                       @RequestParam(value = "direction", defaultValue = "ASC") String direction){
+
+        Page<Categoria> list = categoriaService.findPage(page, linesPerPage, orderBy, direction);
+        Page<CategoriaDTO> listDto = list.map(CategoriaDTO::new);
 
         return ResponseEntity.ok().body(listDto);
     }
